f73a7dc4d41d556c2b092180d77400dc981ad12b
Rajesh Sharma <broncha@rajesharma.com>
2018-05-11 02:31:14 +0545

    Update SyliusSequenceBundle.php

--- SyliusSequenceBundle.php
+++ SyliusSequenceBundle.php
@@ -26,7 +26,7 @@ class SyliusSequenceBundle extends AbstractResourceBundle
     /**
      * {@inheritdoc}
      */
-    public function getSupportedDrivers()
+    public function getSupportedDrivers(): array
     {
         return [
             SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
@@ -46,7 +46,7 @@ class SyliusSequenceBundle extends AbstractResourceBundle
     /**
      * {@inheritdoc}
      */
-    protected function getModelNamespace()
+    protected function getModelNamespace(): ?string
     {
         return 'Sylius\Component\Sequence\Model';
     }
